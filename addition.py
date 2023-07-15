86d8ba63cce088489d84f120a2eb468ea7b0c7e6
unknown <amoldchaudhari@gmail.com>
2023-07-15 12:26:20 +0530

    1st clsroom project

---
+++ addition.py
@@ -0,0 +1,8 @@
+def addition():
+    num1 = int(input("please enter number 1st"))
+    num2 = int(input("please add the 2nd number"))
+    operator=("+","-","*")
+    if (operator == "+"):
+        x=(num1+num2)
+        print(x)
+addition()
